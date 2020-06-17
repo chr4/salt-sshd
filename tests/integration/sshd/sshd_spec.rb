f36ff7fd1fe068a8f38ebb2af13f11b36fde0fff
Chris Aumann <me@chr4.org>
2020-06-17 16:01:01 +0200

    Add integration tests via Github actions

---
+++ tests/integration/sshd/sshd_spec.rb
@@ -0,0 +1,30 @@
+control 'ssh' do
+  title 'should be installed & configured'
+
+  describe package('openssh-server') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/ssh/sshd_config') do
+    its('mode') { should cmp '0644' }
+    its('content') { should match /^\s*Port 22$/ }
+    its('content') { should match /^\s*KexAlgorithms.*curve25519-sha256@libssh.org/ } # Match xenial and bionic versions
+    its('content') { should match /^\s*MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com$/ }
+    its('content') { should match /^\s*Ciphers chacha20-poly1305@openssh.com$/ }
+  end
+
+  describe file('/etc/ssh/moduli') do
+    its('mode') { should cmp '0644' }
+    its('content') { should_not match /\s2047\s/ }
+  end
+
+  describe service('sshd') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe port(22) do
+    it { should be_listening }
+    its('processes') { should include(/sshd/) }
+  end
+end
